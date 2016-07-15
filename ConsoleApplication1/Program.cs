9d2d4ae9b4d8a21307738d43911a870bdfc5e3a8
Elliott-Green <elliottgreen@live.com>
2016-07-15 16:03:54 +0100

    First Commit - Twitter API working - Weather API half working

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tweetinvi;
+using Tweetinvi.Core.Controllers;
+using Tweetinvi.Core.Factories;
+using Tweetinvi.Core.Helpers;
+using Tweetinvi.Models;
+using Tweetinvi.Models.DTO;
+using Tweetinvi.Models.Entities;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            CredentialStrings cs = new CredentialStrings();
+            WeatherStringBuilder wsb = new WeatherStringBuilder();
+
+            Console.WriteLine("Publishing to twitter...");
+
+            Auth.SetUserCredentials(cs.ConsumerKey, cs.ConsumerSecret, cs.AccessToken, cs.AccessTokenSecret);
+
+            
+            m.buildString();
+
+            var  tweet = Tweet.PublishTweet(sb.ToString());
+
+            if (tweet.IsTweetPublished)
+            {
+                Console.WriteLine("Successful");
+            }
+                     
+
+            Console.WriteLine("Any key to Exit...");
+            Console.ReadLine();
+
+        }
+
+
+        public void buildString()
+        {
+
+        }
+    }
+
+    class CredentialStrings
+    {
+        //properties in C#
+        //hash these Twitter hashed strings with BCrypt (32) with my usual password - to make these hashes of hashes
+        //BCrypt will make it (almost) impossible to dehash http://stackoverflow.com/questions/348109/is-double-hashing-a-password-less-secure-than-just-hashing-it-once
+        //will need to dehash too, which could be a problem (dehash means compare, BCrypt is cryptographic hash)
+        public CredentialStrings()
+        {
+            this.ConsumerKey = "K5s3GKFBSIid49rzj0rJgRiXz";
+            this.ConsumerSecret = "VkY604dIFbQQqgX1jwdCynIeZ8SB429M7iGdFZNMQXmfbQ35AZ";
+            this.AccessToken = "415080340-Ko8m0N0PKwQQFP9zuFoUmdUaZxOI1RGP1argAgcb";
+            this.AccessTokenSecret = "ZuAs8Mi4CWw0wl0apIzNitan8DIPnr3mb5YbD8dyaLWjQ";
+        }
+
+        //special behind the scenes syntatic sugar 
+        public string ConsumerKey { get; private set; }
+        public string ConsumerSecret { get; private set; }
+        public string AccessToken { get; private set; }
+        public string AccessTokenSecret { get; private set; }
+
+
+    }
+}
---
+++ ConsoleApplication1/Weather.cs
@@ -0,0 +1,50 @@
+﻿using System.Xml;
+
+namespace ConsoleApplication
+{
+    //http://www.dreamincode.net/forums/topic/153911-c%23using-google-weather-api-in-a-c%23-application/
+
+    class Weather 
+    {
+        //only care about Brighton
+        public const string LOCATION = "Brighton, East Sussex";
+
+        //object that holds the SOAP weather deets
+        
+        
+
+    }
+    public class Conditions
+    {
+        public string AvgTemp = "N/A";
+        public string WindSpeed = "N/A";
+        public string Humidity = "N/A";
+
+        public static Conditions getCurrentConditions(string LOCATION)
+        {
+            Conditions c = new Conditions();
+
+            //potential error with the way im loading string if so -> .Load(string.Format())
+            XmlDocument xmlConditions = new XmlDocument();
+            xmlConditions.Load("http://www.google.com/ig/api?weather=" + LOCATION);
+
+
+            //looking for XML = "/xml_api_reply/weather/forecast_information/WHATEVER VARIABLE YOU NEED HERE"
+            if (xmlConditions.SelectSingleNode("xml_api_reply / weather / problem_cause") != null)
+            {
+                //bomb out 
+                c = null;
+            }
+            else
+            {
+                foreach (XmlNode node in xmlConditions.SelectNodes("/xml_api_reply/weather/forecast_conditions"))
+                {
+                    c.AvgTemp = xmlConditions.SelectSingleNode("/xml_api_reply/weather/forecast_information/temp_c").Attributes["data"].InnerText;
+                    c.WindSpeed = xmlConditions.SelectSingleNode("/xml_api_reply/weather/forecast_information/wind_conditions").Attributes["data"].InnerText;
+                    c.Humidity = xmlConditions.SelectSingleNode("/xml_api_reply/weather/forecast_information/humidity").Attributes["data"].InnerText;
+                }
+            }
+            return c;
+        }
+    }
+}
